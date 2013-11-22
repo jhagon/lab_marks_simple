8e6b0cbed441c95503aac89de1e1a01ddc16c06f
J.P.Hagon <jphagon@gmail.com>
2013-11-22 10:38:39 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LabMarksSimple::Application.config.secret_key_base = 'e382aa69d9ee2658c1f06d067b603859fe38cc135380972c532da55a4db57584784b0c559e51b0689dc1e08958832f1aaf24e4a186b99ee81e8bd61e4940cb70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LabMarksSimple::Application.config.session_store :cookie_store, key: '_lab_marks_simple_session'
